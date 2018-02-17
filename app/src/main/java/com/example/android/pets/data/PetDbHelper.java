46deb9a889b4de5affec28b76208b39195073637
sergeimikhailov <marrog1@mail.ru>
2018-02-17 23:55:30 +0300

    create db helper

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,39 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by KsanaWonka on 17.02.2018.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "pets.db";
+    private static final int DATABASE_VERSION = 1;
+    private final String CREATE_DATABASE_SQL = "CREATE TABLE " + PetContract.PetEntry.TABLE_NAME +
+            " (" + PetContract.PetEntry._ID + " INTEGER, " +
+            PetContract.PetEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+            PetContract.PetEntry.COLUMN_BREED + " TEXT, " +
+            PetContract.PetEntry.COLUMN_GENDER + " INTEGER NOT NULL, " +
+            PetContract.PetEntry.COLUMN_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(CREATE_DATABASE_SQL);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
