53c14b309d23b7526c9a9dbf0d49af57535b7c51
sergeimikhailov <marrog1@mail.ru>
2018-02-17 22:18:52 +0300

    create Contracr for DB

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,33 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by KsanaWonka on 17.02.2018.
+ */
+
+public final class PetContract {
+
+
+    private PetContract(){}
+
+    public static class PetEntry implements BaseColumns{
+
+        public static String TABLE_NAME = "pets";
+        public static String COLUMN_NAME = "name";
+        public static String COLUMN_BREED = "breed";
+        public static String COLUMN_GENDER = "gender";
+        public static String COLUMN_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+
+    }
+
+}
